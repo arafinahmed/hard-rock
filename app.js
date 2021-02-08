7fcc0d7d477c446b62818dd09ce540ba8049cd3b
Arafin Ahmed <arafin2021@gmail.com>
2021-02-08 23:04:11 +0600

    async await

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const lyricsDiv = document.getElementById('songs-lyrics');
 
-const searchSong = () => {
+const searchSong = async() => {
     const searchText = document.getElementById('search-field').value;
     const url = `https://api.lyrics.ovh/suggest/${searchText}`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displaySongs(data.data))
+    const  res = await fetch(url)
+    const data = await res.json();
+    displaySongs(data.data);
 }
 
 const displaySongs = (songs) => {
