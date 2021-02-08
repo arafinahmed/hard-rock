dd57d97ed4fa6901e4fc1d9e7d347faa740451eb
Arafin Ahmed <arafin2021@gmail.com>
2021-02-08 15:21:56 +0600

    search complete

---
+++ app.js
@@ -0,0 +1,26 @@
+const searchSong = () => {
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySongs(data.data))
+}
+
+const displaySongs = (songs) => {
+    const songConatainer = document.getElementById('song-container');
+    songConatainer.innerHTML = "";
+    songs.forEach(song => {
+        const div = document.createElement('div');
+        div.className = "single-result row align-items-center my-3 p-3";
+        div.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">${song.artist.name}</span></p>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songConatainer.appendChild(div);
+        console.log(song);
+    });
+}
